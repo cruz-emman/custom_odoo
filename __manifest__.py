2af5ab91275b282a972a63dbb4b2e0d84d8a94be
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2023-11-24 16:25:50 +0800

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Managment System',
+    'description': """Hospital Manegment""",
+    'depends': [],
+    'data': [
+         'security/ir.model.access.csv',
+         'views/menu.xml',
+         'views/patient.xml'
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_installable': False,
+    'assets': {},
+    'license': 'LGPL-3'
+
+}
